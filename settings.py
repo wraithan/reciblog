5ef2659f7da0aa413e29bd3bbb5d2e99e39d43ae
Chris McDonald <XWraithanX@gmail.com>
2010-11-25 06:38:36 -0800

    lots of small stuff

--- settings.py
+++ settings.py
@@ -90,6 +90,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.admin',
+    'django.contrib.webdesign',
     'blog',
     'recipes',
     'south',
