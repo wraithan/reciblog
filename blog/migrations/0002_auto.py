b465444415ebb4ba0671eb3002df543aaa0433f0
Chris McDonald <XWraithanX@gmail.com>
2010-11-25 05:25:57 -0800

    adding a dependency for a migration

--- blog/migrations/0002_auto.py
+++ blog/migrations/0002_auto.py
@@ -6,6 +6,11 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('recipes', "0001_initial"),
+    )
+
+
     def forwards(self, orm):
         
         # Adding M2M table for field recipes on 'Entry'
