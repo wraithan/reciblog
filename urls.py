9f58883ecb1a207d0a37ac89ae8ad67a2e1889a3
= <=>
2010-11-26 12:41:04 -0800

    Added menu bar and footer, sort of

--- urls.py
+++ urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls.defaults import patterns, url, include
 from django.contrib import admin
 
-
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^', include('reciblog.blog.urls')),
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
